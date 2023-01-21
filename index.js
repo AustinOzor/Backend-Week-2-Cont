1fb3995ad63205faeedf47257bd9d2f5461242c5
Austin Ozor <augustineozor@gmail.com>
2023-01-21 16:47:30 +0300

    updated

--- index.js
+++ index.js
@@ -7,16 +7,18 @@ const handleAllTypesOfRequests = (req, res) => {
     res.send("Response from server.use");
 }
 
-const handlePutRequest = (req, res) => {
+const handlePutProfileRequest = (req, res) => {
     res.send("Put request responded to")
 };
 const handleGetRequest = (req, res) => {
     res.send("Welcome to the login page")
-}
+};
+const handlePutSignUp = (req, res) => {
+    res.send("Welcome to Signup Page")
+};
 server.post('/profile', handleAllTypesOfRequests)
 server.get('/login', handleGetRequest);
-server.put('/profile', handlePutRequest);
-server.put('/signup', (req, res) => {
-    res.send("Welcome to the signup page")
-});
+server.put('/profile', handlePutProfileRequest);
+server.get('/signup',handlePutSignUp);
+server.post('/signup',handlePutSignUp);
 server.listen(3002, () => console.log('server is ready'))
