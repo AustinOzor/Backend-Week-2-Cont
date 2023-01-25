1902212f674374a75016daaf877aa912e0e98ab8
Austin Ozor <augustineozor@gmail.com>
2023-01-25 23:27:02 +0300

    first commit

--- index.js
+++ index.js
@@ -13,12 +13,39 @@ const handlePutProfileRequest = (req, res) => {
 const handleGetRequest = (req, res) => {
     res.send("Welcome to the login page")
 };
-const handlePutSignUp = (req, res) => {
-    res.send("Welcome to Signup Page")
+const handlePutSignUp = (req, res) => {    res.send("Welcome to Signup Page")
 };
-server.post('/profile', handleAllTypesOfRequests)
-server.get('/login', handleGetRequest);
-server.put('/profile', handlePutProfileRequest);
-server.get('/signup',handlePutSignUp);
+
+const middlewarefunction = (req, res,next) =>{
+    console.log("Hello this is our middleware")
+    //console.log(req)
+    //make some checks and if it is true, then next is executed else throw an error
+    next();
+    //middel can send request and make respones
+    //res.send("this is a middleware response")
+}
+//route specific middleware
+const loginRouteSpecificMiddleware = (req, res, next) =>{
+    console.log("login in route middleware executed")
+    next();
+}
+const signupRouteMiddleWare =(req, res, next) => {
+    console.log("signup middleware response")
+    next();
+}
+const profileRouteMiddleWare =(req, res, next) => {
+    console.log("profile middleware is functioning ")
+    next();
+}
+
+//general middlewares
+//server.use(middlewarefunction);
+
+//route middleware
+server.post('/profile',profileRouteMiddleWare ,handleAllTypesOfRequests)
+server.get('/login',loginRouteSpecificMiddleware, handleGetRequest); //middlewares come before request defininstion
+server.put('/profile',profileRouteMiddleWare ,handlePutProfileRequest);
+server.get('/signup', signupRouteMiddleWare, handlePutSignUp);
 server.post('/signup',handlePutSignUp);
+
 server.listen(3002, () => console.log('server is ready'))
